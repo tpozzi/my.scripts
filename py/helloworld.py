a7da108b800b92056519edf965ad27b2e3e4cf3e
mrpozzi <mrpozzi@localhost.localdomain>
2017-01-20 09:09:21 -0500

    python

---
+++ py/helloworld.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#My first python script
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this,"
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
